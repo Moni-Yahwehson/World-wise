99ea4681ddb10724315916cf12c43a197fe02dca
Moni-Yahwehson <moniyahwehson@gmail.com>
2023-11-01 16:41:43 +0100

    Added components and their modules

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,13 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
 
 function Homepage() {
   return (
     <div>
-        <PageNav />
+      <PageNav />
       <h1>WorldWise</h1>
+
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }

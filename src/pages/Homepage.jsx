527222e56d49ec5d57737256967b4d12e31f54d3
Moni-Yahwehson <moniyahwehson@gmail.com>
2023-11-01 15:48:42 +0100

    Building nav bar

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Homepage() {
-    return (
-        <div>
-            WorldWise
-        </div>
-    )
+  return (
+    <div>
+      <h1>WorldWise</h1>
+      <Link to="/pricng">Pricing</Link>
+    </div>
+  );
 }
 
-export default Homepage
+export default Homepage;

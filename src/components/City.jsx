2873962d016f789ab1a2bd0018c8c0ba6a2e4009
Moni-Yahwehson <moniyahwehson@gmail.com>
2023-11-13 11:54:04 +0100

    Made the Application Sharable

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -2,7 +2,7 @@
 // import styles from "./City.module.css";
 // import ButtonBack from "./ButtonBack.module.css";
 
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -14,6 +14,11 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
@@ -23,7 +28,14 @@ function City() {
   };
 
   const { cityName, emoji, date, notes } = currentCity;
-  return <h1>City {id}</h1>;
+  return (
+    <>
+      <h1>City {id}</h1>
+      <p>
+        Position: {lat}, {lng}
+      </p>
+    </>
+  );
 
   // return (
   //   <div className={styles.city}>

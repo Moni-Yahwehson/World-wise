01b21432aa3e7506e8a7147214b695f0f3be1a91
Moni-Yahwehson <moniyahwehson@gmail.com>
2023-11-15 13:28:18 +0100

    Created a cities context api

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -3,9 +3,11 @@ import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
 // eslint-disable-next-line react/prop-types
-function CityList({ cities, isLoading }) {
+function CityList() {
+  const {isLoading, cities} = useCities();
   if (isLoading) return <Spinner />;
 
   // eslint-disable-next-line react/prop-types
